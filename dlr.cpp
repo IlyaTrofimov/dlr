7c46ee3443f58215cb3cb10061ee003734022d37
IlyaTrofimov <i-tr@yandex.ru>
2013-06-13 18:45:25 +0400

    Third dirty version.

--- dlr.cpp
+++ dlr.cpp
@@ -4,6 +4,7 @@
 #include<cstring>
 #include<climits>
 #include<iostream>
+#include<cfloat>
 
 using std::cout;
 
@@ -18,34 +19,49 @@ static inline float soft_threshold(float x, float a) {
 		return x + a;
 }
 
-void update_betaTx(FILE *file, int count, float delta_beta, float *betaTx) {
+char g_buffer[1<<20];
 
-	char buffer[1<<20];
+int parse_line(FILE *file, ulong_t *feature_id, ulong_t *example_id, int *y, float *weight, float *x, fpos_t *fpos) {
 
+	if (fpos)
+		fgetpos(file, fpos);
+
+	if (fgets(g_buffer, 1<<20, file)) {
+		char *pstr = strtok(g_buffer, " ");
+		*feature_id = strtoul(pstr, NULL, 10);
+
+		pstr = strtok(NULL, " ");
+		*example_id = strtoul(pstr, NULL, 10);
+
+		pstr = strtok(NULL, " ");
+		*y = strtol(pstr, NULL, 10);
+
+		pstr = strtok(NULL, " ");
+		*weight = atof(pstr);
+
+		pstr = strtok(NULL, " ");
+		*x = atof(pstr);
+
+		return 1;
+	}
+
+	return 0;
+}
+
+void update_betaTx(FILE *file, int count, float delta_beta, float *betaTx)
+{
 	for (int i = 0; i < count; i++) {
-		if (fgets(buffer, 1<<20, file)) {
+		ulong_t feature_id, example_id;
+		int y;
+		float weight, x; 
 
-			char *pstr = strtok(buffer, " ");
-			ulong_t feature_id = strtoul(pstr, NULL, 10);
-
-			pstr = strtok(NULL, " ");
-			ulong_t example_id = strtoul(pstr, NULL, 10);
-
-			pstr = strtok(NULL, " ");
-			int y = strtol(pstr, NULL, 10);
-
-			pstr = strtok(NULL, " ");
-			float weight = atof(pstr);
-
-			pstr = strtok(NULL, " ");
-			float x = atof(pstr);
-
+		if (parse_line(file, &feature_id, &example_id, &y, &weight, &x, NULL))
 			betaTx[example_id] += delta_beta * x;
-		}
 	}
 }
 
-float get_qloss(float *betaTx, float *betaTx_delta, float *beta, float *beta_new, ulong_t example_count, ulong_t feature_count, float lambda_1, int *all_y) {
+float get_qloss(float *betaTx, float *betaTx_delta, float *beta, float *beta_new, ulong_t example_count, ulong_t feature_count, float lambda_1, int *all_y)
+{
 	float loss = 0.0;
 
 	for (int i = 1; i <= feature_count; i++) 
@@ -69,33 +85,43 @@ float get_qloss(float *betaTx, float *betaTx_delta, float *beta, float *beta_new
 	return loss;
 }
 
-float get_grad(float alpha, float *betaTx, float *betaTx_delta, float *beta, float *beta_new, ulong_t example_count, ulong_t feature_count, float lambda_1, int *all_y) {
+float* get_grad(char *dataset_filename, ulong_t example_count, ulong_t feature_count, float lambda_1, float *beta)
+{
+	float *y_betaTx = (float*)calloc(example_count, sizeof(float));
+	float *grad = (float*)calloc(feature_count, sizeof(float));
+	ulong_t feature_id, example_id;
+	int y;
+	float weight, x; 
 
-	float loss = 0.0;
+	FILE *file = fopen(dataset_filename, "r");
 
-	for (int i = 1; i <= feature_count; i++) {
-		loss += 1.0 / (1.0 + exp(-betaTx[i])) * 
+	while (parse_line(file, &feature_id, &example_id, &y, &weight, &x, NULL)) {
+		y_betaTx[example_id] += y * beta[feature_id] * x;
 	}
 
-	for (int i = 1; i <= example_count; i++) {
-	
-		float logit = -all_y[i] * (betaTx[i] + alpha * betaTx_delta[i]);
-		if (logit > 10)
-			loss += logit;
-		else
-			loss += log(1.0 + exp(logit));
+	rewind(file);
+
+	while (parse_line(file, &feature_id, &example_id, &y, &weight, &x, NULL)) {
+		grad[feature_id] += - y * x / (1.0 + exp(y_betaTx[example_id]));
 	}
 
-	return loss;
+	for (int i = 0; i < feature_count; i++)
+		grad[i] = soft_threshold(grad[i], lambda_1);
+
+	fclose(file);
+	free(y_betaTx);
+
+	return grad;
 }
+
 float get_loss(float alpha, float *betaTx, float *betaTx_delta, float *beta, float *beta_new, ulong_t example_count, ulong_t feature_count, float lambda_1, int *all_y) {
 
 	float loss = 0.0;
 
-	for (int i = 1; i <= feature_count; i++) 
+	for (int i = 0; i <= feature_count; i++) 
 		loss += lambda_1 * fabs((1 - alpha) * beta[i] + alpha * beta_new[i]);
 
-	for (int i = 1; i <= example_count; i++) {
+	for (int i = 0; i <= example_count; i++) {
 	
 		float logit = -all_y[i] * (betaTx[i] + alpha * betaTx_delta[i]);
 		if (logit > 10)
@@ -107,9 +133,32 @@ float get_loss(float alpha, float *betaTx, float *betaTx_delta, float *beta, flo
 	return loss;
 }
 
+void update_feature(FILE *file, ulong_t count, float sum_w_q_x, float sum_w_x_2, float lambda_1, const fpos_t *feature_start_pos, ulong_t prev_feature_id, float *beta_new, float *betaTx_delta)
+{
+	float beta_after_cd = 0.0;
+		
+	if (sum_w_x_2 != 0.0)
+		beta_after_cd = soft_threshold(sum_w_q_x, lambda_1) / sum_w_x_2;
+	else
+		beta_after_cd = 0.0;
+
+	fpos_t cur_fpos;
+	fgetpos(file, &cur_fpos);
+
+	fsetpos(file, feature_start_pos);
+	update_betaTx(file, count, beta_after_cd - beta_new[prev_feature_id], betaTx_delta);
+
+	fsetpos(file, &cur_fpos);
+
+	beta_new[prev_feature_id] = beta_after_cd;
+	//cout << "iter " << iter << " beta " << prev_feature_id << " " << beta_new[prev_feature_id] << "\n";
+
+	//float qloss = get_qloss(betaTx, betaTx_delta, beta, beta_new, example_count, feature_count, lambda_1, all_y);
+	//printf("qloss %f\n", qloss);
+}
+
 int main(int argc, char **argv)
 {
-	char buffer[1<<20];
 	char *dataset_filename = argv[1];
 	ulong_t example_count = strtol(argv[2], NULL, 10);
 	ulong_t feature_count = strtol(argv[3], NULL, 10);
@@ -132,32 +181,31 @@ int main(int argc, char **argv)
 		float sum_w_q_x = 0.0;
 		ulong_t count = 0;
 
-		ulong_t feature_id = 0;
-
 		while (!feof(file)) {
 
 			fpos_t tmp_file_pos;
-			fgetpos(file, &tmp_file_pos);
 
-			if (fgets(buffer, 1<<20, file)) {
+			ulong_t feature_id, example_id;
+			int y;
+			float weight, x; 
 
-				char *pstr = strtok(buffer, " ");
-				feature_id = strtoul(pstr, NULL, 10);
-
-				pstr = strtok(NULL, " ");
-				ulong_t example_id = strtoul(pstr, NULL, 10);
-
-				pstr = strtok(NULL, " ");
-				int y = strtol(pstr, NULL, 10);
-
-				pstr = strtok(NULL, " ");
-				float weight = atof(pstr);
-
-				pstr = strtok(NULL, " ");
-				float x = atof(pstr);
-			
+			while (parse_line(file, &feature_id, &example_id, &y, &weight, &x, &tmp_file_pos)) {
+						
 				if (iter == 1)	
 					all_y[example_id] = y;
+
+				if (feature_id != prev_feature_id) {
+
+					if (prev_feature_id != 0) {
+						update_feature(file, count, sum_w_q_x, sum_w_x_2, lambda_1, &feature_start_pos, prev_feature_id, beta_new, betaTx_delta);
+					}
+
+			 		feature_start_pos = tmp_file_pos;
+
+					sum_w_x_2 = 0.0;
+					sum_w_q_x = 0.0;
+					count = 0;
+				}
 
 				float example_betaTx = betaTx[example_id];
 
@@ -169,87 +217,29 @@ int main(int argc, char **argv)
 					p = 0.999999;
 
 				float w = p * (1 - p);
-				float z = example_betaTx + (((y + 1) / 2) - p) / w;
-
-				if (feature_id != prev_feature_id) {
-
-					if (prev_feature_id != 0) {
-
-						//float qloss1 = get_qloss(betaTx, betaTx_delta, beta, beta_new, example_count, feature_count, lambda_1, all_y);
-						//printf("qloss1 %f\n", qloss1);
-
-						float new_beta = 0.0;
-
-						if (sum_w_x_2 != 0.0)
-							new_beta = soft_threshold(sum_w_q_x, lambda_1) / sum_w_x_2;
-						else
-							new_beta = 0.0;
-					
-						//printf("sum_w_q_x %f sum_w_x_2 %f\n", sum_w_q_x, sum_w_x_2);	
-
-						// rewind to the start of current feature block, write new (beta, x) for examples
-						fpos_t cur_fpos;
-						fgetpos(file, &cur_fpos);
-
-						fsetpos(file, &feature_start_pos);
-						update_betaTx(file, count, new_beta - beta_new[prev_feature_id], betaTx_delta);
-
-						fsetpos(file, &cur_fpos);
-
-						beta_new[prev_feature_id] = new_beta;
-						//cout << "iter " << iter << " beta " << prev_feature_id << " " << beta_new[prev_feature_id] << "\n";
-
-						//float qloss = get_qloss(betaTx, betaTx_delta, beta, beta_new, example_count, feature_count, lambda_1, all_y);
-						//printf("qloss %f\n", qloss);
-
-					}
-
-			 		feature_start_pos = tmp_file_pos;
-
-					sum_w_x_2 = 0.0;
-					sum_w_q_x = 0.0;
-					count = 0;
-				}
-
-				count++;
+				int y01 = (y + 1) / 2;
+				float z = example_betaTx + (y01 - p) / w;
 
 				float q = z - (example_betaTx + betaTx_delta[example_id] - beta_new[feature_id] * x);
 				sum_w_x_2 += w * x * x;
 				sum_w_q_x += w * q * x;
 			
+				count++;
 				prev_feature_id = feature_id;
 			}
 		}
 
-		//
-		float new_beta = 0.0;
-		
-		if (sum_w_x_2 != 0.0)
-			new_beta = soft_threshold(sum_w_q_x, lambda_1) / sum_w_x_2;
-		else
-			new_beta = 0.0;
-
-		fpos_t cur_fpos;
-		fgetpos(file, &cur_fpos);
-
-		fsetpos(file, &feature_start_pos);
-		update_betaTx(file, count, new_beta - beta_new[prev_feature_id], betaTx_delta);
-
-		fsetpos(file, &cur_fpos);
-
-		beta_new[prev_feature_id] = new_beta;
-		//cout << "iter " << iter << " beta " << prev_feature_id << " " << beta_new[prev_feature_id] << "\n";
-
-		//float qloss = get_qloss(betaTx, betaTx_delta, beta, beta_new, example_count, feature_count, lambda_1, all_y);
-		//printf("qloss %f\n", qloss);
+		update_feature(file, count, sum_w_q_x, sum_w_x_2, lambda_1, &feature_start_pos, prev_feature_id, beta_new, betaTx_delta);
 
 		//
-		float min_loss = 1.0e6;
+		// Linear search
+		//
+		float min_loss = FLT_MAX;
 		float best_alpha = 0.0;
 
 		float alpha_list[] = {1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0};
 
-		for (int i = 0; i < 11; i++) {
+		for (int i = 0; i < sizeof(alpha_list) / sizeof(float); i++) {
 			float loss = get_loss(alpha_list[i], betaTx, betaTx_delta, beta, beta_new, example_count, feature_count, lambda_1, all_y);
 			//printf("alpha %f loss %f\n", alpha_list[i], loss);
 
@@ -259,15 +249,18 @@ int main(int argc, char **argv)
 			}
 		}
 
+		//
+		// Make step
+		//
 		int next_newton = (iter % 5 == 0);
 
 		if (next_newton) {
-			for (int i = 1; i <= example_count; i++) {
+			for (int i = 0; i <= example_count; i++) {
 				betaTx[i] += best_alpha * betaTx_delta[i];
 				betaTx_delta[i] = 0.0;
 			}
 
-			for (int i = 1; i <= feature_count; i++) {
+			for (int i = 0; i <= feature_count; i++) {
 				beta[i] = (1 - best_alpha) * beta[i] + best_alpha * beta_new[i];
 				beta_new[i] = beta[i];
 			}
@@ -276,7 +269,19 @@ int main(int argc, char **argv)
 		else {
 			cout << "CD best_alpha " << best_alpha << " min_loss " << min_loss << "\n";
 		}
-	
+
+		//
+		// Print gradient info
+		//
+		float *grad = get_grad(dataset_filename, example_count, feature_count, lambda_1, beta_new);
+
+		cout << "grad ";
+		for (int i = 0; i <= feature_count; i++)
+			printf("%f ", grad[i]);
+
+		free(grad);
+
+		cout << "\n";
 		cout << "\n";
  
 		if (next_newton && (best_alpha == 0.0))
@@ -286,7 +291,7 @@ int main(int argc, char **argv)
 	}
 
 	//
-	//
+	// Writing features to file
 	//
 	FILE *file_rfeatures = fopen("rfeatures", "w");
 
